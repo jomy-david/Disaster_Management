b4e5c241db939dbf8e9640ed143d26e6d4467913
Asura <jomydkizhakeyil@gmail.com>
2023-08-06 23:00:01 +0530

    created Person Table

--- Main/admin.py
+++ Main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Person
 
 # Register your models here.
+
+class adminPerson(admin.ModelAdmin):
+    list_display=[
+        'name','age','camp_id','contact','gender'
+    ]
+
+admin.site.register(Person,adminPerson)
--- Main/models.py
+++ Main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+    camp_id = models.CharField(max_length=3)
+    contact = models.CharField(max_length=10)
+    gender = models.CharField(max_length=7)
